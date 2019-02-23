05042a5805db2c12623e25482a333f01188a2878
Matt Clark <matthewclark989@boisestate.edu>
2019-02-22 17:33:00 -0700

    better model (gamma)

--- Cleaning_first_cut.R
+++ Cleaning_first_cut.R
@@ -7,11 +7,16 @@ lgd2<-lgd[c(2:6,19:22)]
 names(lgd2)[2:9]<-c("Going","Interested","Views","Reached","Type","Co_hosts","Spent","Sales")
 lgd2<-lgd2[-5]
 lgd2<-na.omit(lgd2)
-fit<-glm(Sales~Spent+Views, data=lgd2 )
+
+fit<-glm(Sales~Spent+Views,family=Gamma(link="log") ,data=lgd2 )
+fit<-glm(Sales~Spent+Views ,data=lgd2 )
+AIC(fit)
+#The gamma model appears to be better
 summary(fit)
 plot(Sales~Spent,data=lgd2)
 ggplot(data=lgd2,aes(x=Views, y=Sales))+
   scale_x_log10()+
 geom_point()+
 geom_smooth(method=lm)+theme_classic()
-2+2
+summary(fit)
+plogis(0.0005396)
