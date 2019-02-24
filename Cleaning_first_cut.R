83d72c6e056921b90068488d357063fdcfa3ea9f
Matt Clark <matthewclark989@boisestate.edu>
2019-02-24 16:30:43 -0700

    interpreting

--- Cleaning_first_cut.R
+++ Cleaning_first_cut.R
@@ -1,22 +1,35 @@
 library(readr)
 library(ggplot2)
+library(rstanarm)
+library(lme4)
 lgd<-read_csv("~/Lost_grove/Facebook_Spending.csv")
 lgd$Spent[is.na(lgd$Spent)] <- 0
 lgd$`Co-Hosts`[is.na(lgd$`Co-Hosts`)] <- 0
 lgd2<-lgd[c(2:6,19:22)]
 names(lgd2)[2:9]<-c("Going","Interested","Views","Reached","Type","Co_hosts","Spent","Sales")
-lgd2<-lgd2[-5]
+#lgd2<-lgd2[-5]
 lgd2<-na.omit(lgd2)
-
-fit<-glm(Sales~Spent+Views,family=Gamma(link="log") ,data=lgd2 )
-fit<-glm(Sales~Spent+Views ,data=lgd2 )
+lgd2$multi_host<-as.logical(ifelse(lgd2$Co_hosts == 0,0,1))
+lgd2<-lgd2[1:106,]
+lgd2$paid<-as.logical(ifelse(lgd2$Spent == 0,0,1))
+#try to remove outlier
+lgd2<-lgd2[-17,]
+#for a gamma distributionwith log-link use exp to interpret parameters
+fit<-glm(Sales~Interested,family=Gamma(link="log") ,data=lgd2 )
+summary(fit)
+coef(fit)
+fit<-glm(Sales~Spent+Reached+Type,family=Gamma(link="log") ,data=lgd2)
+loo(fit)
 AIC(fit)
 #The gamma model appears to be better
 summary(fit)
-plot(Sales~Spent,data=lgd2)
-ggplot(data=lgd2,aes(x=Views, y=Sales))+
+plot(Sales~Reached,data=lgd2)
+ggplot(data=lgd2,aes(x=Reached, y=Sales))+
   scale_x_log10()+
 geom_point()+
 geom_smooth(method=lm)+theme_classic()
 summary(fit)
-plogis(0.0005396)
+
+ggplot(data=na.omit(lgd2),aes(x=multi_host,y=Sales))+geom_boxplot()+theme_classic()
+ggplot(data=lgd2,aes(x=Type,y=Sales))+geom_boxplot()+theme_classic()
+
